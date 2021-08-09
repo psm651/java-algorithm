239d9f2cff393b7f92c44aa7e2cce21aaaeadc7a
seongmin-park <psm651@mailplug.co.kr>
2021-08-09 17:54:13 +0900

    백준 입출력 마지막

---
+++ src/step1/bj10869/Main.java
@@ -0,0 +1,21 @@
+package step1.bj10869;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.System.in;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String[] line = br.readLine().split(" ");
+        Integer a = Integer.valueOf(line[0]);
+        Integer b = Integer.parseInt(line[1]);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
