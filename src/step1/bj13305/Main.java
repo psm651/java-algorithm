ea75b1f0b34b7dcb107fab3a9ade21a9757b7315
psm651 <psm651@naver.com>
2021-10-20 22:08:16 +0900

    백준 그리디완료

--- src/step1/bj13305/Main.java
+++ src/step1/bj13305/Main.java
@@ -12,34 +12,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        oilPrice = new long[N + 1];
-        distanceList = new long[N];
+        oilPrice = new long[N];
+        distanceList = new long[N-1];
         StringTokenizer distanceToken = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer oilToken = new StringTokenizer(br.readLine(), " ");
-        //배열선언 안하고도 가능할듯?
-        for (int i = 0; i < N; i++) {
-            oilPrice[i] = Long.valueOf(oilToken.nextToken());
+        for (int j = 0; j < N-1; j++) {
+            distanceList[j] = Long.valueOf(distanceToken.nextToken());
         }
-        for (int i = 0; i <N-1 ; i++) {
-            distanceList[i] = Long.valueOf(distanceToken.nextToken());
+        StringTokenizer oilPriceToken = new StringTokenizer(br.readLine(), " ");
+        for (int j = 0; j < N; j++) {
+            oilPrice[j] = Long.valueOf(oilPriceToken.nextToken());
         }
-        int location = 0;
-        int nextLocation = 1;
-        int result = 0;
-        while (location<N-1) {
-            if (nextLocation == N - 1) {
-                for (int i = location; i < nextLocation - location; i++) {
-                    result += (oilPrice[location] * distanceList[i]);
-                }
+        int start = 0;
+        long result=0;
+        for (int j = 1; j < N; j++) {
+            result += oilPrice[start] * distanceList[j - 1];
+            if (oilPrice[start] > oilPrice[j]) {
+                start = j;
             }
-            if (oilPrice[location] <= oilPrice[nextLocation]) {
-            } else {
-                for (int i = location; i < nextLocation - location; i++) {
-                    result += (oilPrice[location] * distanceList[i]);
-                }
-                location = nextLocation;
-            }
-            nextLocation++;
         }
         System.out.println(result);
     }
