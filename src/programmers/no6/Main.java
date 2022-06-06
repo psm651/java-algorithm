3e174a47c58926d4b8c55d37a231b15e24f7a339
psm651 <psm651@naver.com>
2022-06-06 14:54:03 +0900

    프로그래머스 키패드 누르기

--- src/programmers/no6/Main.java
+++ src/programmers/no6/Main.java
@@ -2,19 +2,46 @@ package programmers.no6;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        //int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+//        String hand = "right";
+//        int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+//        String hand = "left";
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         String hand = "right";
-        String answer = "";
-        for (int i = 0; i < numbers.length; i++) {
-            int number = numbers[i];
-            if (number == 1 || number == 4 || number == 7) {
-                answer += "L";
-            } else if (number == 3 || number == 6 || number == 9) {
-                answer += "R";
-            } else {
+        StringBuilder sb = new StringBuilder();
+        int left=10, right=12;
+        int L_dis, R_dis;
+        for(int n : numbers) {
+            if(n == 1 || n == 4 || n == 7) {
+                sb.append("L");
+                left = n;
 
+            }else if(n == 3 || n == 6 || n == 9) {
+                sb.append("R");
+                right = n;
+
+            }else {
+                if(n==0) n += 11;
+                L_dis = (Math.abs(n-left))/3 + (Math.abs(n-left))%3;
+                R_dis = (Math.abs(right-n))/3 + (Math.abs(right-n))%3;
+                if(L_dis == R_dis) {
+                    if(hand.equals("right")) {
+                        sb.append("R");
+                        right = n;
+                    }else {
+                        sb.append("L");
+                        left = n;
+                    }
+                }else if(L_dis > R_dis) {
+                    sb.append("R");
+                    right = n;
+                }else {
+                    sb.append("L");
+                    left = n;
+                }
             }
         }
+        System.out.println(sb.toString());
 
     }
 }
