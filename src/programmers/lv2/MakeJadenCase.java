394d97323cfbf12e61a5f510a1d7c06b4427cd45
bbakseong <psm651@naver.com>
2022-10-29 18:47:15 +0900

    level-up

--- src/programmers/lv2/MakeJadenCase.java
+++ src/programmers/lv2/MakeJadenCase.java
@@ -4,17 +4,20 @@ package programmers.lv2;
 public class MakeJadenCase {
     public static void main(String[] args) {
         StringBuilder answer = new StringBuilder();
-        String s = "3people unFollowed me";
-        String[] sArr = s.split(" ");
-        for (int i = 0; i < sArr.length; i++) {
-            if (Character.isDigit(sArr[i].charAt(0))) {
-                continue;
+        String s = "3people  unFollowed me  ";
+        String[] data = s.toLowerCase().split("");
+        boolean isUpperCase = true;
+        for (String datum : data) {
+            if (isUpperCase) {
+                answer.append(datum.toUpperCase());
+            } else {
+                answer.append(datum);
             }
-            sArr[i] = Character.toUpperCase(sArr[i].charAt(0)) + sArr[i].substring(1).toLowerCase();
-        }
-        answer.append(sArr[0]);
-        for (int l = 1; l <sArr.length ; l++) {
-            answer.append(" ").append(sArr[l]);
+            if (datum.equals(" ")) {
+                isUpperCase = true;
+            }else {
+                isUpperCase = false;
+            }
         }
         System.out.println(answer);
     }
